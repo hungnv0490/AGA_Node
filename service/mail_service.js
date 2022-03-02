4eb2e3ed379d48dd3f90687c2bfc26bb6ccd1498
Hung <hungnv0490@gmail.com>
2022-03-02 15:44:21 +0700

    no message

--- service/mail_service.js
+++ service/mail_service.js
@@ -4,11 +4,26 @@ const myRedis = require('../myredis.js');
 var logger = log4js.getLogger();
 const mySqlDb = require('../mysqldb.js');
 const mailService = express.Router();
+const verifyTokenBlockchain = require('../middlewares/verifyToken.js');
 
-mailService.post('/add', async (req, res) => {
+const UNAME_TO_UID = "uname_to_uid";
+
+mailService.post('/add',verifyTokenBlockchain, async (req, res) => {
     var json = req.body;
     logger.info(JSON.stringify(json));
-    mySqlDb.addMailBox(json.title, json.content, -1, json.userId, json.rewards, json.isRead, json.isReceived, function (code) {
+    var receiverId = await myRedis.hGet(UNAME_TO_UID, json.Receiver);
+    var sender = -1;
+    if(json.Sender != "-1"){
+        sender = await myRedis.hGet(UNAME_TO_UID, json.Sender);
+    }
+    if(!sender || !receiverId){
+        var dataRes = {}
+        dataRes.code = 101;
+        res.send(dataRes);
+        logger.info("mail_service sender:" + sender + " receiver:" + receiverId);
+        return;
+    }
+    mySqlDb.addMailBox(json.title, json.content, sender, receiverId, json.rewards, 0, 1, function (code) {
         var dataRes = {}
         dataRes.code = code;
         res.send(dataRes);
