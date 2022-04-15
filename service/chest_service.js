88825bc33538c6b5456d674beeee929d8f0ad7ab
Hung <hungnv0490@gmail.com>
2022-04-15 23:51:11 +0700

    up

--- service/chest_service.js
+++ service/chest_service.js
@@ -10,74 +10,96 @@ const mySqlDb = require('../mysqldb.js');
 const myRedis = require('../myredis.js');
 var Chest = require('../entities/chest.js');
 
-chestService.get('/get', async (req, res) => {
-    var dataRes = {}
-    dataRes.code = 200;
-    dataRes.data = chestConfig.toApiRes();
-    res.send(dataRes);
-});
+// chestService.get('/get', async (req, res) => {
+//     var dataRes = {}
+//     dataRes.code = 200;
+//     dataRes.data = chestConfig.toApiRes();
+//     res.send(dataRes);
+// });
 
-chestService.post('/set', async (req, res, next) => {
-    try {
-        logger.info(req.body);
-        var json = req.body;
-        chestConfig.DailyLoginMaxChestPoint = json.DailyLoginMaxChestPoint;
-        chestConfig.DailyMissionMaxChestPoint = json.DailyMissionMaxChestPoint;
-        chestConfig.AchievementMaxChestPoint = json.AchievementMaxChestPoint;
-        chestConfig.chests = []
-        chestConfig.dailyLoginPacks = []
+// chestService.post('/set', async (req, res, next) => {
+//     try {
+//         logger.info(req.body);
+//         var json = req.body;
+//         chestConfig.DailyLoginMaxChestPoint = json.DailyLoginMaxChestPoint;
+//         chestConfig.DailyMissionMaxChestPoint = json.DailyMissionMaxChestPoint;
+//         chestConfig.AchievementMaxChestPoint = json.AchievementMaxChestPoint;
+//         chestConfig.chests = []
+//         chestConfig.dailyLoginPacks = []
 
-        for(var chest of json.chests){
-            var packCardObs = [];
-            for(var packCard of chest.packCards){
-                var packCardOb = new PackCard(packCard.Common, packCard.UnCommon, packCard.Rare, packCard.Epic, packCard.Legend);
-                packCardObs.push(packCardOb);
-            }
-            chestConfig.chests.push(new Chest(packCardObs));
-        }
-        for(var chest of json.dailyLoginPacks){
-            var packCardObs = [];
-            for(var packCard of chest.packCards){
-                var packCardOb = new PackCard(packCard.Common, packCard.UnCommon, packCard.Rare, packCard.Epic, packCard.Legend);
-                packCardObs.push(packCardOb);
-            }
-            chestConfig.dailyLoginPacks.push(new Chest(packCardObs));
-        }
+//         for(var chest of json.chests){
+//             var packCardObs = [];
+//             for(var packCard of chest.packCards){
+//                 var packCardOb = new PackCard(packCard.Common, packCard.UnCommon, packCard.Rare, packCard.Epic, packCard.Legend);
+//                 packCardObs.push(packCardOb);
+//             }
+//             chestConfig.chests.push(new Chest(packCardObs));
+//         }
+//         for(var chest of json.dailyLoginPacks){
+//             var packCardObs = [];
+//             for(var packCard of chest.packCards){
+//                 var packCardOb = new PackCard(packCard.Common, packCard.UnCommon, packCard.Rare, packCard.Epic, packCard.Legend);
+//                 packCardObs.push(packCardOb);
+//             }
+//             chestConfig.dailyLoginPacks.push(new Chest(packCardObs));
+//         }
 
-        await chestConfig.setConfig();
-        var dataRes = {}
-        dataRes.code = 200;
-        dataRes.data = chestConfig.toApiRes();
-        res.send(dataRes);
-        // res.send(chestConfig.toJson(chestObs));
-    } catch (error) {
-        next(error);
-    }
-});
+//         await chestConfig.setConfig();
+//         var dataRes = {}
+//         dataRes.code = 200;
+//         dataRes.data = chestConfig.toApiRes();
+//         res.send(dataRes);
+//         // res.send(chestConfig.toJson(chestObs));
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+// chestService.get('/user/:username', async (req, res, next) => {
+//     try {
+//         var dataRes = {};
+//         var packs = [];
+//         var username = mySqlDb.escape(req.params.username);
+//         var sql = `SELECT up.*, p.pack_cards FROM aga.user_pack up
+//     left join pack p
+//     on up.pack_id = p.id
+//     where up.user_id = (select user_id from users where username = ${username});`;
+//         mySqlDb.query(sql, function (err, result, fields) {
+//             if (result != null && result.length > 0) {
+//                 for (var rs of result) {
+//                     var packCards = []
+//                     var packCardStr_arr = rs.pack_cards.split('|');
+//                     for (var packCardStr of packCardStr_arr) {
+//                         var cards = packCardStr.split('-');
+//                         packCards.push(new PackCard(parseFloat(cards[0]), parseFloat(cards[1]), parseFloat(cards[2]), parseFloat(cards[3]), parseFloat(cards[4])));
+//                     }
+//                     var pack = new Pack2(packCards, rs.amount, rs.pack_id);
+//                     packs.push(pack);
+//                 }
+//             }
+//             dataRes.packs = packs;
+//             dataRes.code = 200;
+//             res.send(dataRes);
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 chestService.get('/user/:username', async (req, res, next) => {
     try {
         var dataRes = {};
         var packs = [];
         var username = mySqlDb.escape(req.params.username);
-        var sql = `SELECT up.*, p.pack_cards FROM aga.user_pack up
-    left join pack p
-    on up.pack_id = p.id
-    where up.user_id = (select user_id from users where username = ${username});`;
+        var sql = `SELECT amount from user_pack
+                where user_id = (select user_id from users where username = ${username});`;
         mySqlDb.query(sql, function (err, result, fields) {
-            if (result != null && result.length > 0) {
-                for (var rs of result) {
-                    var packCards = []
-                    var packCardStr_arr = rs.pack_cards.split('|');
-                    for (var packCardStr of packCardStr_arr) {
-                        var cards = packCardStr.split('-');
-                        packCards.push(new PackCard(parseFloat(cards[0]), parseFloat(cards[1]), parseFloat(cards[2]), parseFloat(cards[3]), parseFloat(cards[4])));
-                    }
-                    var pack = new Pack2(packCards, rs.amount, rs.pack_id);
-                    packs.push(pack);
-                }
+            logger.info(result);
+            var amount = 0;
+            if (result && result.length > 0) {
+                amount = result[0].amount;
             }
-            dataRes.packs = packs;
+            dataRes.amount = amount;
             dataRes.code = 200;
             res.send(dataRes);
         });
@@ -86,16 +108,90 @@ chestService.get('/user/:username', async (req, res, next) => {
     }
 });
 
+// chestService.post('/user/claim', verifyToken, async (req, res, next) => {
+//     try {
+//         var dataRes = {};
+//         var username = mySqlDb.escape(req.body.username);
+//         var usernameStr = username.replaceAll("'", "");
+//         // var pack = mySqlDb.escape(req.body.pack);
+//         // pack = pack.replaceAll("'", "");
+//         // var fm = res
+//         var packId = req.body.packId;
+//         var amount = req.body.amount;
+//         const UNAME_TO_UID = "uname_to_uid";
+//         var uid = await myRedis.hGet(UNAME_TO_UID, usernameStr);
+//         if (!uid) {
+//             dataRes.code = 201;
+//             dataRes.msg = "user not exist in game";
+//             res.send(dataRes);
+//             return;
+//         }
+//         var key = `username-pack-claim-lock:${usernameStr}`;
+//         var value = await myRedis.get(key);
+//         if (value != null && value == "true") {
+//             dataRes.code = 202;
+//             dataRes.msg = "please try again";
+//             res.send(dataRes);
+//             return;
+//         }
+//         await myRedis.set(key, true);
+//         await myRedis.expire(key, 1);
+
+//         var sql = `Select count(*) as x From aga.user_pack
+//                     where user_id = ${uid} And pack_id = ${packId};`;
+//         mySqlDb.query(sql, async function (err, result, fields) {
+//             if (err) {
+//                 logger.error(err);
+//                 dataRes.code = 203;
+//                 dataRes.msg = err.message;
+//                 res.send(dataRes);
+//                 await myRedis.set(key, false);
+//             }
+//             else {
+//                 var max = result[0].x;
+//                 if (amount > max) {
+//                     dataRes.code = 204;
+//                     dataRes.msg = "too max amount, max is:" + max;
+//                     res.send(dataRes);
+//                     await myRedis.set(key, false);
+//                 }
+//                 else {
+//                     sql = `Update aga.user_pack up set amount=amount-${amount}  
+//                             where up.user_id = ${uid} And pack_id = ${packId} And amount >= ${amount};`;
+//                     mySqlDb.query(sql, async function (err, result, fields) {
+//                         if (err) {
+//                             logger.error(err);
+//                             dataRes.code = 101;
+//                             dataRes.msg = err.message;
+//                         }
+//                         else {
+//                             dataRes.code = 200;
+//                         }
+//                         res.send(dataRes);
+//                         await myRedis.set(key, false);
+//                     });
+//                 }
+//             }
+//         });
+
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
 chestService.post('/user/claim', verifyToken, async (req, res, next) => {
     try {
         var dataRes = {};
         var username = mySqlDb.escape(req.body.username);
         var usernameStr = username.replaceAll("'", "");
-        // var pack = mySqlDb.escape(req.body.pack);
-        // pack = pack.replaceAll("'", "");
-        // var fm = res
-        var packId = req.body.packId;
+        // var packId = req.body.packId;
         var amount = req.body.amount;
+        if(isNaN(amount) || amount <= 0){
+            dataRes.code = 205;
+            dataRes.msg = "amount error";
+            res.send(dataRes);
+            return;
+        }
         const UNAME_TO_UID = "uname_to_uid";
         var uid = await myRedis.hGet(UNAME_TO_UID, usernameStr);
         if (!uid) {
@@ -115,8 +211,7 @@ chestService.post('/user/claim', verifyToken, async (req, res, next) => {
         await myRedis.set(key, true);
         await myRedis.expire(key, 1);
 
-        var sql = `Select count(*) as x From aga.user_pack
-                    where user_id = ${uid} And pack_id = ${packId};`;
+        var sql = `Select amount as x From aga.user_pack where user_id = ${uid};`;
         mySqlDb.query(sql, async function (err, result, fields) {
             if (err) {
                 logger.error(err);
@@ -135,7 +230,7 @@ chestService.post('/user/claim', verifyToken, async (req, res, next) => {
                 }
                 else {
                     sql = `Update aga.user_pack up set amount=amount-${amount}  
-                            where up.user_id = ${uid} And pack_id = ${packId} And amount >= ${amount};`;
+                            where up.user_id = ${uid} And amount >= ${amount};`;
                     mySqlDb.query(sql, async function (err, result, fields) {
                         if (err) {
                             logger.error(err);
@@ -144,6 +239,7 @@ chestService.post('/user/claim', verifyToken, async (req, res, next) => {
                         }
                         else {
                             dataRes.code = 200;
+                            dataRes.amount_leave = max - amount;
                         }
                         res.send(dataRes);
                         await myRedis.set(key, false);
@@ -176,8 +272,8 @@ chestService.post('/add-pack-user', verifyToken, async (req, res, next) => {
         (user_id,
         pack_id,
         amount,create_time,is_new)
-        VALUES (${uid}, ${req.body.packId}, ${req.body.packAmount},'${dateTimeStr}',${1})
-        ON DUPLICATE KEY UPDATE amount=amount+${req.body.packAmount},is_new=1;`;
+        VALUES (${uid}, ${1}, ${req.body.amount},'${dateTimeStr}',1)
+        ON DUPLICATE KEY UPDATE amount=amount+${req.body.amount},is_new=1;`;
         mySqlDb.execute(sql, (err, result, fields)=>{
             var dataRes = {}
             dataRes.code = 200;
