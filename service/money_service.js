495388236e8f7636c3fb002c0d462477b78d7290
Hung <hungnv0490@gmail.com>
2022-04-21 20:52:37 +0700

    up

--- service/money_service.js
+++ service/money_service.js
@@ -21,7 +21,7 @@ moneyService.get('/get/:username', async (req, res, next) => {
         var sql = `set @err = 0;
     set @userId = 0;
     set @diamond = 0;
-    call aga.SP_GetMoney(${username}, '{${date}}', @err, @userId, @diamond);
+    call aga.SP_GetMoneyFromPool(${username}, '{${date}}', @err, @userId, @diamond);
     select @err, @userId, @diamond;
     `;
         mySqlDb.query(sql, function (err, result, fields) {
@@ -32,60 +32,53 @@ moneyService.get('/get/:username', async (req, res, next) => {
                 var userId = result[result.length - 1][0]['@userId'];
                 dataRes.code = err;
                 dataRes.diamond = diamond;
-
-                sql = `Select *, now() nw from users where user_id = ${userId}`;
-                var nextSecond = 0;
-                var times = 0;
-                mySqlDb.query(sql, (err, result, fields) => {
-                    // logger.info(result);
-                    logger.info(JSON.stringify(result));
-                    if (!err && result.length > 0) {
-                        var dt = result[0];
-                        times = dt.withdraw_times;
-                        var lastDate = new Date(dt.withdraw_last_date);
-                        logger.info(lastDate);
-                        if (lastDate.getDate() == new Date().getDate()
-                            && lastDate.getMonth() == new Date().getMonth()
-                            && lastDate.getFullYear() == new Date().getFullYear()) {
-                            if (times < 5) {
-                                var nextDate = new Date(lastDate.getTime() + 30 * 60 * 1000);
-                                var ms = nextDate.getTime() - new Date().getTime();
-                                if (ms <= 0) nextSecond = 0;
-                                else nextSecond = Math.round(ms / 1000);
-                            }
-                            else {
-                                var curTime = new Date().getTime();
-                                var nextDate = new Date(curTime + 24 * 60 * 60 * 1000);
-                                var startNextDate = util.dateFormat(nextDate, "%Y-%m-%d", false);
-                                startNextDate += " 00:00:00";
-                                logger.info("startNextDate:" + startNextDate);
-                                var ms = new Date(startNextDate).getTime() - curTime;
-                                nextSecond = Math.round(ms / 1000);
-                            }
-                        }
-                        else {
-                            times = 0;
-                            nextSecond = 0;
-                            // var curTime = new Date().getTime();
-                            // var nextDate = new Date(curTime + 24 * 60 * 60 * 1000);
-                            // var startNextDate = util.dateFormat(nextDate, "%Y-%m-%d", false);
-                            // startNextDate += " 00:00:00";
-                            // logger.info("startNextDate:" + startNextDate);
-                            // var ms = new Date(startNextDate).getTime() - curTime;
-                            // nextSecond = Math.round(ms / 1000);
-                        }
-                    }
-                    dataRes.times = times;
-                    dataRes.nextSecond = nextSecond;
-                    res.send(dataRes);
-                    logger.info("money_service get:" + JSON.stringify(dataRes));
-                });
+                res.send(dataRes);
+                // sql = `Select *, now() nw from users where user_id = ${userId}`;
+                // var nextSecond = 0;
+                // var times = 0;
+                // mySqlDb.query(sql, (err, result, fields) => {
+                //     // logger.info(result);
+                //     logger.info(JSON.stringify(result));
+                //     if (!err && result.length > 0) {
+                //         var dt = result[0];
+                //         times = dt.withdraw_times;
+                //         var lastDate = new Date(dt.withdraw_last_date);
+                //         logger.info(lastDate);
+                //         if (lastDate.getDate() == new Date().getDate()
+                //             && lastDate.getMonth() == new Date().getMonth()
+                //             && lastDate.getFullYear() == new Date().getFullYear()) {
+                //             if (times < 5) {
+                //                 var nextDate = new Date(lastDate.getTime() + 30 * 60 * 1000);
+                //                 var ms = nextDate.getTime() - new Date().getTime();
+                //                 if (ms <= 0) nextSecond = 0;
+                //                 else nextSecond = Math.round(ms / 1000);
+                //             }
+                //             else {
+                //                 var curTime = new Date().getTime();
+                //                 var nextDate = new Date(curTime + 24 * 60 * 60 * 1000);
+                //                 var startNextDate = util.dateFormat(nextDate, "%Y-%m-%d", false);
+                //                 startNextDate += " 00:00:00";
+                //                 logger.info("startNextDate:" + startNextDate);
+                //                 var ms = new Date(startNextDate).getTime() - curTime;
+                //                 nextSecond = Math.round(ms / 1000);
+                //             }
+                //         }
+                //         else {
+                //             times = 0;
+                //             nextSecond = 0;
+                //         }
+                //     }
+                //     dataRes.times = times;
+                //     dataRes.nextSecond = nextSecond;
+                //     res.send(dataRes);
+                //     logger.info("money_service get:" + JSON.stringify(dataRes));
+                // });
             }
             else {
                 dataRes.code = 600;
                 dataRes.diamond = 0;
-                dataRes.times = 0;
-                dataRes.nextSecond = 0;
+                // dataRes.times = 0;
+                // dataRes.nextSecond = 0;
                 res.send(dataRes);
                 logger.info("money_service get:" + JSON.stringify(dataRes));
             }
@@ -196,7 +189,7 @@ moneyService.post('/withdraw', verifyTokenBlockchain, async (req, res, next) => 
         var curDate = util.dateFormat(new Date(), "%Y-%m-%d %H:%M:%S", false);
         var sql = `set @err = 0;
     Set @userId = 0;
-    call aga.SP_WithDraw(${username}, ${req.body.diamond}, '${date}', '${curDate}', @userId,@err);
+    call aga.SP_WithDrawFromPool(${username}, ${req.body.diamond}, '${date}', '${curDate}', @userId,@err);
     select @userId,@err;
     `;
         mySqlDb.query(sql, async function (err, result, fields) {
@@ -210,29 +203,29 @@ moneyService.post('/withdraw', verifyTokenBlockchain, async (req, res, next) => 
 
                 await myRedis.set(key, false);
 
-                var withdrawGroupBot = await myRedis.get("withdrawGroupBot");
-                if (withdrawGroupBot) {
-                    var groups = withdrawGroupBot.split("|");
-                    var url = `https://api.telegram.org/bot${process.env.agaWidthdrawBot}/sendMessage`;
-                    var content = `${usernameStr} widthraw ${req.body.diamond} diamond`;
-                    for (var group of groups) {
-                        logger.info("group:" + group);                      
-                        if (group) {
-                            axios.post(url, {
-                                chat_id: group,
-                                text: content
-                              })
-                              .then(function (response) {
-                                // logger.error(response);
-                              })
-                              .catch(function (error) {
-                                  if(error.response){
-                                    logger.error(error.response.data);
-                                  }
-                              });
-                        }
-                    }
-                }
+                // var withdrawGroupBot = await myRedis.get("withdrawGroupBot");
+                // if (withdrawGroupBot) {
+                //     var groups = withdrawGroupBot.split("|");
+                //     var url = `https://api.telegram.org/bot${process.env.agaWidthdrawBot}/sendMessage`;
+                //     var content = `${usernameStr} widthraw ${req.body.diamond} diamond`;
+                //     for (var group of groups) {
+                //         logger.info("group:" + group);                      
+                //         if (group) {
+                //             axios.post(url, {
+                //                 chat_id: group,
+                //                 text: content
+                //               })
+                //               .then(function (response) {
+                //                 // logger.error(response);
+                //               })
+                //               .catch(function (error) {
+                //                   if(error.response){
+                //                     logger.error(error.response.data);
+                //                   }
+                //               });
+                //         }
+                //     }
+                // }
 
                 if (err == 200) {
                     var UPDATE_MONEY = "update-money";
